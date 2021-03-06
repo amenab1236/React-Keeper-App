dea2afe41f237d502f7003b8dabd59d1a1b3480b
amenab1236 <68471726+amenab1236@users.noreply.github.com>
2021-03-06 11:05:40 +0000

    react keeper app

---
+++ build/precache-manifest.82418fee4d02db04b69eb3ad89762828.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66be93069425900f565c22acff125cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716e347a9f896567d424",
+    "url": "/static/js/2.a112150c.chunk.js"
+  },
+  {
+    "revision": "6862c5ff3797929004e7",
+    "url": "/static/js/main.0cb4a5d0.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
